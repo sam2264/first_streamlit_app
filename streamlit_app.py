a7ac525d58308062d62c215efa9937d6da4d6eed
sam2264 <112367220+sam2264@users.noreply.github.com>
2022-08-29 22:15:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.subheader('Breakfast menu')
 streamlit.caption('Omega 3 and Blue berry Oat Meal')
+streamlit.caption('Kale, Spinach & Rocket smoothie')
 streamlit.caption('Hard Boiled Free Range Egg')
